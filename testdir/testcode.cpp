25014508b9cb1c4cdc1867a0da65e473f193fdbc
Fabian Soh <fabiansoh@gmail.com>
2018-10-09 17:24:26 +0800

    Add newFunction3 in testcode.cpp

---
+++ testdir/testcode.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main ()
+{
+    cout <<"Hello World!" <<endl;
+}
+
+int newFunction ()
+{
+    return 0;
+}
+
+int newFunction2 ()
+{
+    return 0;
+}
+
+int newFunction3 ()
+{
+    return 0;
+}
