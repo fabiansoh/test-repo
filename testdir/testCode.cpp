246147ca5ce0dc1a844d16a480519b8d9ba525a1
Fabian Soh <fabiansoh@gmail.com>
2018-10-12 09:58:47 +0800

    Rename testcode.cpp to testCode.cpp

---
+++ testdir/testCode.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main ()
+{
+    cout <<"Hello World!" <<endl;
+}
+
+int newFunction2 ()
+{
+    return 0;
+}
+
+int newFunction3 ()
+{
+    return 0;
+}
+
+int newFunction4 ()
+{
+    return 0;
+}
+
+int newFunction5 ()
+{
+    return 0;
+}
+
+int newFunction6 ()
+{
+    return 0;
+}
