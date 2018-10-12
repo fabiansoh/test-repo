a4f4a302c9e45ca4504b27e2ab75cbacc8ba11f8
Fabian Soh <fabiansoh@gmail.com>
2018-10-12 09:59:41 +0800

    Add testCode3.cpp

---
+++ testdir/testCode3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+// This is testCode3.cpp
+
+using namespace std;
+
+int main ()
+{
+    cout <<"Hello World!" <<endl;
+}
+
+int newFunction2 ()
+{
+    return 0;
+}
+
+int newFunction3 ()
+{
+    return 0;
+}
+
+int newFunction4 ()
+{
+    return 0;
+}
+
+int newFunction5 ()
+{
+    return 0;
+}
